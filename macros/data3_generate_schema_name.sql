b948ae5f181d2c3ede30e329b4c58c0d215b2261
Tofan Sahoo <tofan.sahoo@three.co.uk>
2022-08-21 20:56:54 +0000

    projectinitialized

---
+++ macros/data3_generate_schema_name.sql
@@ -0,0 +1,16 @@
+-- Override schema name depending on target environment
+
+{% macro generate_schema_name(custom_schema_name, node) -%}
+
+    {%- set default_schema = target.schema -%}
+    {%- if custom_schema_name is none -%}
+
+        {{ default_schema }}
+
+    {%- else -%}
+              
+        {{ custom_schema_name | trim }}
+        
+    {%- endif -%}
+
+{%- endmacro %}
